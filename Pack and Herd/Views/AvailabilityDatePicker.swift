670938707e7f1ada9f6180e8e03698a4f302bbd3
Cameron Watson <cameronw@watts-ondevelopment.com>
2017-12-02 22:41:54 -0500

    create

--- Pack and Herd/Views/AvailabilityDatePicker.swift
+++ Pack and Herd/Views/AvailabilityDatePicker.swift
@@ -32,19 +32,39 @@ class AvailabilityDatePicker: UIPickerView, UIPickerViewDelegate, UIPickerViewDa
     //MARK: PickerViewDataSource Methods
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         if date {
-            
+            return 3
         }else{
-            
+            return 2
         }
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        <#code#>
+        if date {
+            switch(component){
+            case 0: // Month
+                return 12
+                break;
+            case 1: // Day
+                return 31
+                return 7
+                break;
+            case 2: // Year
+                return 2
+                break;
+            default:
+                return 0
+                break;
+            }
+        }else{
+            return 2
+        }
     }
     
     //MARK: PickerViewDelegate Methods
     
     //MARK: Firestore Methods
-    
+    private func getEventTimes(){
+        
+    }
     
 }
